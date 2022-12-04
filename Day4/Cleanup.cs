f899bfaece58ff34ea0a4b91a25a19a7a8004167
Jukka Teittinen <jukka@teittinen.dev>
2022-12-04 11:39:56 +0200

    Small fix

--- Day4/Cleanup.cs
+++ Day4/Cleanup.cs
@@ -16,17 +16,15 @@ public static class Cleanup
     
     public static int AssignmentOverlaps(string path)
     {
-        var result = File
+        return File
             .ReadLines(path)
             .Select(l => l.Trim())
             .Select(l => l.Split('-', ','))
             .Select(l => new ElfPair(new ElfAssignment(int.Parse(l[0]), int.Parse(l[1])),
                 new ElfAssignment(int.Parse(l[2]), int.Parse(l[3]))))
-            .Where(pair => (pair.first.min <= pair.second.min && pair.first.max >= pair.second.max) // Contains
+            .Count(pair => (pair.first.min <= pair.second.min && pair.first.max >= pair.second.max) // Contains
                            || (pair.second.min <= pair.first.min && pair.second.max >= pair.first.max) // Contains
                            || (pair.first.max >= pair.second.min && pair.first.min < pair.second.min) // Overlaps
                            || (pair.second.max >= pair.first.min && pair.second.min < pair.first.min)); // Overlaps
-
-        return result.Count();
     }
 }
